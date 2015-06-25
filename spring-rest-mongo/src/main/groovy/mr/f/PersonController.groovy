a5097607346b0bf16de7de80bf3d87f111d63ae8
Dennis Frommknecht <dev@frommknecht.net>
2015-06-25 21:34:48 +0200

    Added missing security annotations

--- spring-rest-mongo/src/main/groovy/mr/f/PersonController.groovy
+++ spring-rest-mongo/src/main/groovy/mr/f/PersonController.groovy
@@ -8,20 +8,24 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.access.prepost.PreAuthorize
 
 @RestController
 @Slf4j
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequestMapping("/persons")
 class PersonController {
     @Autowired
     private PersonRepository repository
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     Person getPersonById(@PathVariable String id) {
         log.info("getPersonById with parameter $id")
         repository.findOne(id)
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(method = RequestMethod.GET)
     List<Person> getAllPersons() {
         log.info("getAllPersons()")
@@ -48,6 +52,7 @@ class PersonController {
         repository.delete(id)
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(value = "/search/byFirstName/{firstName}", method = RequestMethod.GET)
     Person getPersonByFirstName(@PathVariable String firstName) {
         log.info("getPersonByFirstName with parameter $firstName")
--- spring-rest-mongo/src/main/groovy/mr/f/SecurityConfig.groovy
+++ spring-rest-mongo/src/main/groovy/mr/f/SecurityConfig.groovy
@@ -3,11 +3,13 @@ package mr.f
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
